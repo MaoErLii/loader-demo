3b3e12378296ce8673c004b8a7af929f266a4fde
MaoErLii <2844735407@qq.com>
2020-01-02 16:27:24 +0800

    A 测试用例更新

--- test/loader-name.test.js
+++ test/loader-name.test.js
@@ -1,7 +1,7 @@
 import compiler from './compiler';
 
 test('Inserts name and outputs JavaScript', async () => {
-  const stats = await compiler('example.txt', {name: 'Alice'});
+  const stats = await compiler('example.txt', {name: 'Sakura'});
   const output = stats.toJson().modules[0].source;
 
   expect(output).toBe('export default "Hey Sakura\\n"');
